f434714a0b7064e04a72a95cbc819f916647af0a
ahunkele <hunkeleandrew@gmail.com>
2025-09-04 11:19:15 -0500

    init

---
+++ S3BackendPhotoUpload/src/main/java/com/java_backend/S3BackendPhotoUpload/S3BackendPhotoUploadApplication.java
@@ -0,0 +1,13 @@
+package com.java_backend.S3BackendPhotoUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3BackendPhotoUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3BackendPhotoUploadApplication.class, args);
+	}
+
+}
---
+++ S3BackendPhotoUpload/src/test/java/com/java_backend/S3BackendPhotoUpload/S3BackendPhotoUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_backend.S3BackendPhotoUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3BackendPhotoUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
